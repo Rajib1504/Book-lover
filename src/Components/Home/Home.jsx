6d62e51617f1909820357c07dc831c5d62af7e33
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-11-01 13:25:43 +0530

    banner done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,10 @@
+import Banner from "../Banner/Banner";
+
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-center">I'm the Home page</h1>
+    <div className="my-2">
+      {/* <h1 className="text-2xl font-bold text-center">I'm the Home page</h1> */}
+      <Banner></Banner>
     </div>
   );
 };
