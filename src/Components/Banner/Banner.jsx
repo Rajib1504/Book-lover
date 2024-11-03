096ef1e4ee8fa9d087e756bd2b16e1d28a4a2cd7
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-11-03 17:06:24 +0530

    need implement

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,11 +6,16 @@ const Banner = () => {
       <div className="hero-content  flex-col lg:flex-row-reverse">
         <img className="w-[20%]" src={bannerImg} />
         <div className="w-full sm:w-1/2 text-center sm:text-left">
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <h1 className="text-5xl font-bold">
+            Books to freshen up your bookshelf
+          </h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Discover a curated collection of books that will revitalize your
+            bookshelf with fresh insights and compelling narratives. From
+            timeless classics to modern must-reads, these titles offer stories
+            that captivate and inspire. Whether you're in search of adventure,
+            wisdom, or relaxation, these books are sure to elevate your reading
+            experience.
           </p>
           <button className="btn btn-outline btn-success">
             <Link to={"/list"}>Get Started</Link>
